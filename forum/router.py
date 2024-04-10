8e3a08556fbdfaefd8a2ce16ec96fb7e38ae9393
Aleksei Nushtaev <nushtaev2110@gmail.com>
2024-04-10 09:11:10 +0500

    Add patch views

--- forum/router.py
+++ forum/router.py
@@ -1,7 +1,7 @@
 from typing import List, Union
 
 from fastapi import APIRouter, Depends
-from sqlalchemy import select, insert, delete
+from sqlalchemy import select, insert, delete, update
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
@@ -9,7 +9,7 @@ from auth.base_config import current_user
 from auth.models import User
 from database import get_async_session
 from forum.models import Post, Comment
-from forum.schemas import PostRead, PostCreate, CommentCreate, OnePostRead
+from forum.schemas import PostRead, PostCreate, CommentCreate, OnePostRead, PostUpdate, CommentUpdate
 
 router = APIRouter(
     prefix="/forum",
@@ -65,11 +65,55 @@ async def delete_post(post_id: int,
     else:
         return f"You don't have post with id {post_id}"
 
+@router.patch("/post/{post_id}")
+async def patch_post(post_id: int,
+                     update_post: PostUpdate,
+                     user: User = Depends(current_user),
+                     session: AsyncSession = Depends(get_async_session)):
+    stmt = select(Post).where(Post.id == post_id, Post.user_id == user.id)
+    result = await session.execute(stmt)
+    if result.scalars().first():
+        stmt = update(Post).where(Post.id == post_id).values(**update_post.dict(exclude_unset=True))
+        await session.execute(stmt)
+        await session.commit()
+        return {"status": "post was updated"}
+    else:
+        return f"You don't have post with id {post_id}"
+
 @router.post("/add_comment")
-async def add_post(new_comment: CommentCreate,
+async def add_comment(new_comment: CommentCreate,
                    user: User = Depends(current_user),
                    session: AsyncSession = Depends(get_async_session)):
     comment = insert(Comment).values(**new_comment.dict(), user_id=user.id)
     await session.execute(comment)
     await session.commit()
     return {"status": "comment was created"}
+
+@router.delete("/comment_id/{comment_id}")
+async def delete_comment(comment_id: int,
+                         user: User = Depends(current_user),
+                         session: AsyncSession = Depends(get_async_session)):
+    stmt = select(Comment).where(Comment.id == comment_id, Comment.user_id == user.id)
+    result = await session.execute(stmt)
+    if result.scalars().first():
+        stmt = delete(Comment).where(Comment.id == comment_id)
+        await session.execute(stmt)
+        await session.commit()
+        return {"status": "comment was deleted"}
+    else:
+        return f"You don't have comment with id {comment_id}"
+
+@router.patch("/comment/{comment_id}")
+async def patch_comment(comment_id: int,
+                        update_comment: CommentUpdate,
+                        user: User = Depends(current_user),
+                        session: AsyncSession = Depends(get_async_session)):
+    stmt = select(Comment).where(Comment.id == comment_id, Comment.user_id == user.id)
+    result = await session.execute(stmt)
+    if result.scalars().first():
+        stmt = update(Comment).where(Comment.id == comment_id).values(**update_comment.dict(exclude_unset=True))
+        await session.execute(stmt)
+        await session.commit()
+        return {"status": "comment was updated"}
+    else:
+        return f"You don't have comment with id {comment_id}"
--- forum/schemas.py
+++ forum/schemas.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel
 
@@ -8,14 +8,26 @@ class PostCreate(BaseModel):
     title: str
     text: str
 
+
+class PostUpdate(BaseModel):
+    title: Optional[str] = None
+    text: Optional[str] = None
+
+
 class PostRead(PostCreate):
     id: int
     created_at: datetime
     user_id: int
 
+
 class CommentCreate(BaseModel):
     post_id: int
     text: str
+
+
+class CommentUpdate(BaseModel):
+    text: Optional[str] = None
+
 
 class CommentRead(CommentCreate):
     id: int
@@ -23,8 +35,6 @@ class CommentRead(CommentCreate):
     text: str
     created_at: datetime
 
+
 class OnePostRead(PostRead):
     comments: List[CommentRead]
-
-
-
